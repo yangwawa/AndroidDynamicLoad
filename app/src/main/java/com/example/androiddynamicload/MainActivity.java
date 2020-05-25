5e283980afffa0b6653ab7428e32a0f305cc1708
weijingdong <weijingdong@bytedance.com>
2020-05-25 19:10:10 +0800

    加载apk资源的代码

--- app/src/main/java/com/example/androiddynamicload/MainActivity.java
+++ app/src/main/java/com/example/androiddynamicload/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.androiddynamicload;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -10,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     TextView mTv = null;
     Button mBtn = null;
+    Button mBtnLoadApk = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 File sdcardPath = Environment.getExternalStorageDirectory().getAbsoluteFile();
-//                String pluginName = "myplugin.jar";
                 String pluginName = "myplugin_dex.jar";
                 String pluginPath = sdcardPath.getAbsolutePath() + File.separator + pluginName;
 //                PathClassLoader classLoader = new PathClassLoader(pluginPath, getClass().getClassLoader());
@@ -62,5 +67,68 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        mBtnLoadApk = findViewById(R.id.button2);
+        mBtnLoadApk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    File sdcardPath = Environment.getExternalStorageDirectory().getAbsoluteFile();
+                    String pluginApk = "mypluginapk-debug.apk";
+                    String pluginPath = sdcardPath.getAbsolutePath() + File.separator + pluginApk;
+
+                    int resId = getAppNameResId(pluginPath);
+                    Resources resources = getResourcesObject(MainActivity.this, pluginPath);
+                    String apkName = resources.getText(resId).toString();
+
+                    Log.v(TAG, "apkName=" + apkName);
+                    mTv.setText("apkName=" + apkName);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
+
+    public int getAppNameResId(String apkPath){
+        PackageManager packageManager = this.getPackageManager();
+        PackageInfo info = packageManager.getPackageArchiveInfo(apkPath, 0);
+        return info.applicationInfo.labelRes;
+
+    }
+
+    /**
+     * 获取res中text
+     */
+    public static String loadTextFromRes(Context context, Resources resources, int resId){
+        if(context == null || resources == null || resId == 0){
+            return null;
+        }
+        CharSequence appName = null;
+        try {
+            appName = resources.getText(resId);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return appName != null ? appName.toString() : null;
+    }
+
+    /**
+     * 获去apk的res
+     */
+    public static Resources getResourcesObject(Context context, String apkPath) throws Exception {
+        Resources res = context.getResources();
+
+        Class<?> assertClass = Class.forName("android.content.res.AssetManager");
+        Object assetMag  = assertClass.getConstructor(null).newInstance(null);
+        Method method = assertClass.getMethod("addAssetPath", String.class);
+        method.invoke(assetMag, apkPath);
+
+        Class<?> resClass = Class.forName("android.content.res.Resources");
+        res = (Resources) resClass.getConstructor(assertClass, res.getDisplayMetrics().getClass(), res.getConfiguration().getClass())
+                .newInstance(assetMag, res.getDisplayMetrics(), res.getConfiguration());
+        return res;
+    }
+
 }
--- mypluginapk/src/main/java/com/example/mypluginapk/MainActivity.java
+++ mypluginapk/src/main/java/com/example/mypluginapk/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mypluginapk;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public int sum(int a, int b){
+        return a + b;
     }
 }
